183fd9b8c68e2e4ed2325c20d5e18b49e151a495
Primo <pm.lyszczarz@gmail.com>
2013-06-08 01:21:14 +0200

    ContextSimilarity metric implementation.

---
+++ SynomymExtractor/src/main/java/model/metrics/ContextSimilarity.java
@@ -0,0 +1,77 @@
+package model.metrics;
+
+import java.util.*;
+
+/**
+ */
+public class ContextSimilarity {
+
+    public static final int DEFAULT_CONTEXT_RADIUS = 2;
+
+    public List<EntitySimilarity> getSynonyms (List<List<String>> text) {
+        return getSynonyms(text, DEFAULT_CONTEXT_RADIUS);
+    }
+
+
+    public List<EntitySimilarity> getSynonyms (List<List<String>> text, final int contextRadius) {
+        HashMap<String, HashSet<String>> contextMap = new HashMap<String, HashSet<String>>();
+
+        for(List<String> l : text) {
+            for( String token : l) {
+                HashSet<String> contexts = null;
+                if (!contextMap.containsKey(token)) {
+                    contexts = new HashSet<String>();
+                    contextMap.put(token, contexts);
+                }
+                contexts = contextMap.get(token);
+                ArrayList<String> newContexts = getContexts(token, l, contextRadius);
+                contexts.addAll(newContexts);
+            }
+        }
+
+        List<EntitySimilarity> result = new ArrayList<EntitySimilarity>();
+        String[] words = contextMap.keySet().toArray(new String[0]);
+
+        for (int i = 0; i < words.length; ++i)
+            for (int j = i+1; j< words.length; ++j) {
+                double similarity = getRelation(words[i], words[j], contextMap);
+                EntitySimilarity e = new EntitySimilarity(words[i],words[j], similarity);
+                result.add(e);
+            }
+        return result;
+    }
+
+
+    private ArrayList<String> getContexts(String token, List<String> tokens, final int contextRadius) {
+        ArrayList<String> results = new ArrayList<String>();
+        final int index = tokens.indexOf(token);
+        final int listLimit = tokens.size()-1;
+        int i = index - contextRadius;
+        i = (i>=0) ? i:0;
+        int k = index + contextRadius;
+        k = (k <= tokens.size()-1) ? k:listLimit;
+        for (int j = i; j <= k;++j) {
+            String temp = tokens.get(j);
+            if (j!=index) {
+                // Ignoring temp = token
+                results.add(temp);
+            }
+        }
+        return results;
+    }
+
+    private double getRelation(String string, String string2,
+                                HashMap<String, HashSet<String>> contextMap) {
+        HashSet<String> c1 = contextMap.get(string);
+        HashSet<String> c2 = contextMap.get(string2);
+
+        Set<String> intersection = new HashSet<String>(c1);
+        Set<String> union = new HashSet<String>(c1);
+
+        intersection.retainAll(c2);
+        union.addAll(c2);
+
+        return ((double)intersection.size())/((double)union.size());
+
+    }
+}
